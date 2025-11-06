a663cdd2011a4858604a069bdf51b5c5fdf1429a
AdamImd <43316251+AdamImd@users.noreply.github.com>
2025-11-06 18:38:04 +0000

    Deploying to gh-pages from @ AdamImd/AdamImd.github.io@863b3f53fc27a844eaa3ef6125232f404fbe3e84 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,8 +451,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-spark-remote",
+          title: 'SPARK Remote',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
@@ -523,13 +523,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
